abfb5f3fdfb472ccd4f6616ddae2c33c79dae970
caixioajia <caixiaojia@xiaomi.com>
2018-02-01 08:31:25 +0800

    新增zepto基础函数

--- zepto_bak.js
+++ zepto_bak.js
@@ -7,7 +7,11 @@ var Cai = (function() {
     emptyArray = [],
     document = window.document,
     slice = emptyArray.slice,
-    simpleSelectorRE = /^[\w-]*$/
+    concat = emptyArray.concat,
+    every = emptyArray.every,
+    simpleSelectorRE = /^[\w-]*$/,
+    class2type={},
+    toString=class2type.toString
 
   function Z(dom, selector) {
     var i, len = dom ? dom.length : 0
@@ -17,10 +21,17 @@ var Cai = (function() {
     this.selector = selector|| ''
     this.length = len
   }
-  
-  zepto.isZ = function (object) {
-    return object instanceof Zepto.Z
+
+  function likeArray(obj) {
+    var length = !!obj && 'length' in obj && obj.length,
+      type = $.type(obj)
+    return 'function' != type && ('array' == type || length === 0 || (typeof length == 'number' && length > 0 && (length - 1) in obj))
   }
+
+  function flatten(array) {
+    return array.length > 0 ? $.fn.concat.apply([], array) : array
+  }
+
   
   zepto.qsa = function (element, selector) {
     var found,
@@ -49,6 +60,10 @@ var Cai = (function() {
     return new Z(dom, selector)
   }
 
+  zepto.isZ = function (object) {
+    return object instanceof Z
+  }
+
   zepto.init = function (selector, context) {
     var dom
     if (!selector) {
@@ -68,6 +83,9 @@ var Cai = (function() {
     return zepto.Z(dom, selector)
   }
 
+  zepto.matches = function (elements, selector) {
+  }
+
   $ = function (selector, context) {
     return zepto.init(selector, context)
   }
@@ -80,14 +98,93 @@ var Cai = (function() {
     sort: emptyArray.sort,
     splice: emptyArray.splice,
     indexOf: emptyArray.indexOf,
+    slice: slice,
     get: function (idx) {
       return idx === undefined ? slice.call(this) : this[idx >= 0 ? idx : idx + this.length]
+    },
+    toArray: function () {
+      return this.get()
+    },
+    concat: function () {
+      var i, value, args = []
+      for (i = 0; i < arguments.length; i ++ ) {
+        value = arguments[i]
+        args[i] = zepto.isZ(value) ? value.toArray() : value
+      }
+      return concat.apply(zepto.isZ(this) ? this.toArray() : this, args)
+    },
+    map: function (fn) {
+      return $($.map(this, function (el, i) {
+        return fn.call(el, i , el)
+      }))
+    },
+    ready: function (callback) {
+      if (document.readyState === 'complete' || (document.readyState !=='loading' && !document.documentElement.doScroll)) {
+        setTimeout(function () {
+          callback($)
+        }, 0)
+      } else {
+        var handler = function () {
+          document.removeEventListener('DOMContentLoaded', handler, false)
+          window.removeEventListener('load', handler, false)
+          callback($)
+        }
+      }
+      document.addEventListener('DOMContentLoaded', handler, false)
+      window.addEventListener('load', handler, false)
+    },
+    size: function () {
+      return this.length
+    },
+    each: function (callback) {
+      every.call(this, function (el, index) {
+        return callback(el, index)!== false
+      })
+      return this
+    },
+    remove: function () {
+      this.each(function () {
+        if (this.parentNode != null) {
+          this.parentNode.removeChild(this)
+        }
+      })
+    },
+    filter: function () {
+      
     }
-    // concat: function () {
-    // }
+
   }
 
-  Z.prototype = $.fn
+
+  "Boolean Number String Function Array Date RegExp Object Error".split(" ").forEach(function (item, index, arr) {
+    class2type[ "[object " + item + "]" ] = item.toLowerCase()
+  })
+
+  $.type = function (obj) {
+    return obj == null ? String(obj) :
+      class2type[toString.call(obj)] || "object"
+  }
+
+  $.map = function (elements, callback) {
+    var value, values = [], i, key
+    if (likeArray(elements))
+      for (i = 0;i < elements.length; i ++) {
+        value = callback(elements[i], i)
+        if (value != null) values.push(value)
+      } else {
+      for (key in elements) {
+        value = callback(elements[key], key)
+        if (value != null) values.push(value)
+      }
+    }
+    return flatten(values)
+  }
+
+  $.isFunction =  function (value) {
+    return $.type(value) == 'function'
+  }
+
+  zepto.Z.prototype = Z.prototype = $.fn
 
   return $
 })()
